a6af64d224ae08ac89353f4d1f63e551f1c835ac
Arnav Mangal <mangal.arnav@gmail.com>
2022-06-26 17:30:54 -0700

    trying out some changes

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex');
+const dotenv = require('dotenv').config();
 
 const register = require('./controllers/register');
 const signin = require('./controllers/signin');
@@ -13,7 +14,11 @@ const db = knex({
     client: 'pg',
     connection: {
         connectionString : process.env.DATABASE_URL,
-        ssl: true,
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+          }
+        // ssl: true,
     }
 });
 
@@ -23,36 +28,7 @@ const app = express();
 app.use(express.json()); //helps convert frontend user inputted data to understandable json
 app.use(cors()); //allows requested resources on a web server
 
-// const database = {
-//     users: [
-//         {
-//             id: '123',
-//             name: 'John',
-//             password: 'cookies',
-//             email: 'john@gmail.com',
-//             entries: 0,
-//             joined: new Date()
-//         },
-//         {
-//             id: '124',
-//             name: 'Sally',
-//             password: 'bananas',
-//             email: 'sally@gmail.com',
-//             entries: 0,
-//             joined: new Date()
-//         }
-//     ],
-
-//     login: [
-//         {
-//             id: '987',
-//             hash: '',
-//             email: 'john@gmail.com'
-//         }
-//     ]
-// }
-
-app.get('/', (req, res) => { res.send('success'); })
+app.get('/', (req, res) => { res.send('success') });
 
 app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) })
 
@@ -64,7 +40,7 @@ app.put('/image', (req, res) => { image.handleImage(req, res, db) });
 
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) });
 
-app.listen(process.env.PORT || 3000, () => { console.log(`app is running on port ${process.env.PORT}`) })
+app.listen(process.env.PORT || 3000, () => { console.log(`app is running on port ${process.env.PORT}`) });
 
 /*PLAN
 / --> res = this is working
